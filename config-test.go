2f5a90fa6fa1bcdad3861aef8818f0709e6fef6e
Ramin Akhbari <ramin@akhbari.us>
2022-09-27 00:31:52 -0700

    Fixed tab chars

--- config-test.go
+++ config-test.go
@@ -5,13 +5,13 @@ import (
     "log"
     "os"
 
-	"github.com/AbsaOSS/env-binder/env"
-	"gopkg.in/yaml.v3"
+    "github.com/AbsaOSS/env-binder/env"
+    "gopkg.in/yaml.v3"
 )
 
 type AppConfig1 struct {
     ServerConfig struct {
-        Host        string 	`yaml:"host"`
+        Host        string  `yaml:"host"`
         Port        int     `yaml:"port"`
         TlsPort     int     `yaml:"tlsPort"`
         DummyPort   int     `yaml:"dummyPort"`
@@ -20,7 +20,7 @@ type AppConfig1 struct {
 
 type AppConfig2 struct {
     ServerConfig struct {
-        Host        string 	`yaml:"host" env:"MY_HOST"`
+        Host        string  `yaml:"host" env:"MY_HOST"`
         Port        int     `yaml:"port" env:"MY_PORT"`
         TlsPort     int     `yaml:"tlsPort" env:"MY_TLS_PORT"`
         DummyPort   int     `yaml:"dummyPort" env:"MY_DUMMY_PORT"`
@@ -29,7 +29,7 @@ type AppConfig2 struct {
 
 type AppConfig3 struct {
     ServerConfig struct {
-        Host        string 	`env:"MY_HOST" yaml:"host"`
+        Host        string  `env:"MY_HOST" yaml:"host"`
         Port        int     `env:"MY_PORT" yaml:"port"`
         TlsPort     int     `env:"MY_TLS_PORT" yaml:"tlsPort"`
         DummyPort   int     `env:"MY_DUMMY_PORT" yaml:"dummyPort"`
@@ -37,51 +37,51 @@ type AppConfig3 struct {
 }
 
 func processError(err error) {
-	fmt.Println(err)
-	os.Exit(2)
+    fmt.Println(err)
+    os.Exit(2)
 }
 
 func readFile(cfg interface{}) {
     configFile := "config.yaml"
-	log.Printf("Reading App config from %s ...\n", configFile)
-	f, err := os.Open(configFile)
-	if err != nil {
-		processError(err)
-	}
-	defer f.Close()
+    log.Printf("Reading App config from %s ...\n", configFile)
+    f, err := os.Open(configFile)
+    if err != nil {
+        processError(err)
+    }
+    defer f.Close()
 
-	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(cfg)
-	if err != nil {
-		processError(err)
-	}
+    decoder := yaml.NewDecoder(f)
+    err = decoder.Decode(cfg)
+    if err != nil {
+        processError(err)
+    }
 }
 
 func main() {
     cfg1 := &AppConfig1{}
-	readFile(cfg1)
+    readFile(cfg1)
 
     cfg2 := &AppConfig2{}
-	readFile(cfg2)
+    readFile(cfg2)
 
     cfg3 := &AppConfig3{}
-	readFile(cfg3)
+    readFile(cfg3)
 
-	if err := env.Bind(cfg1); err != nil {
-		fmt.Println(err)
-		return
+    if err := env.Bind(cfg1); err != nil {
+        fmt.Println(err)
+        return
     }
-	log.Printf("App config 1: %+v\n", cfg1)
+    log.Printf("App config 1: %+v\n", cfg1)
 
-	if err := env.Bind(cfg2); err != nil {
-		fmt.Println(err)
-		return
+    if err := env.Bind(cfg2); err != nil {
+        fmt.Println(err)
+        return
     }
-	log.Printf("App config 2: %+v\n", cfg2)
+    log.Printf("App config 2: %+v\n", cfg2)
     
-	if err := env.Bind(cfg3); err != nil {
-		fmt.Println(err)
-		return
+    if err := env.Bind(cfg3); err != nil {
+        fmt.Println(err)
+        return
     }
-	log.Printf("App config 3: %+v\n", cfg3)
+    log.Printf("App config 3: %+v\n", cfg3)
 }
